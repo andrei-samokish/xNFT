4ced8f0c116919c33e02669b58e1058a01eba8e4
ThomasAqu1nas <andranikoganyan27@gmail.com>
2023-10-14 01:16:25 +0700

    feat: add mainnet alchemy provider

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "@nomicfoundation/hardhat-ethers";
 import dotenv from "dotenv";
 dotenv.config();
 
+const alchemyApiKeyGoerli = process.env.ALCHEMY_API_KEY_TESTNET as string;
 const alchemyApiKey = process.env.ALCHEMY_API_KEY as string;
 const privateKey = process.env.PRIVATE_KEY as string;
 
@@ -11,7 +12,11 @@ const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${alchemyApiKey}`,
+      url: `https://eth-goerli.g.alchemy.com/v2/${alchemyApiKeyGoerli}`,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${alchemyApiKey}`,
       accounts: [privateKey],
     },
   },
