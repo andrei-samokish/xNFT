058028c2cb811ff13ad94b7cc22c6222f3ba84ee
ThomasAqu1nas <andranikoganyan27@gmail.com>
2023-10-14 01:17:35 +0700

    fix: minor

--- deployment/bridgeSendReceiveDeploy.ts
+++ deployment/bridgeSendReceiveDeploy.ts
@@ -11,101 +11,59 @@ async function main() {
   let alchemyProvider: JsonRpcProvider;
   let deployer: Wallet;
   let zkEvmDeployer: Wallet;
-  let bridgeAddress: string;
+  let bridgeAddress = "0xF6BEEeBB578e214CA9E23B0e9683454Ff88Ed2A7";
   const networkType = process.env.NETWORK_TYPE as string;
   const privateKey = process.env.PRIVATE_KEY as string;
-  alchemyProvider = new ethers.AlchemyProvider(
-    "goerli",
-    process.env.ALCHEMY_API_KEY as string
-  );
+
   if (networkType === "testnet") {
-    bridgeAddress =
-      "0xF6BEEeBB578e214CA9E23B0e9683454Ff88Ed2A7";
-    zkEVMProvider = new ethers.JsonRpcProvider(
-      process.env.ZK_EVM_TESTNET_RPC as string
-    );
-    deployer = new ethers.Wallet(
-      privateKey,
-      alchemyProvider
-    );
-    zkEvmDeployer = new ethers.Wallet(
-      privateKey,
-      zkEVMProvider
-    );
+    alchemyProvider = new ethers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY as string);
+    zkEVMProvider = new ethers.JsonRpcProvider(process.env.ZK_EVM_TESTNET_RPC as string);
+    deployer = new ethers.Wallet(privateKey, alchemyProvider);
+    zkEvmDeployer = new ethers.Wallet(privateKey, zkEVMProvider);
   } else if (networkType === "mainnet") {
-    zkEVMProvider = new ethers.JsonRpcProvider(
-      process.env.ZK_EVM_RPC as string
-    );
-    deployer = new ethers.Wallet(
-      privateKey,
-      alchemyProvider
-    );
-    zkEvmDeployer = new ethers.Wallet(
-      privateKey,
-      zkEVMProvider
-    );
+    alchemyProvider = new ethers.AlchemyProvider("mainnet", process.env.ALCHEMY_API_KEY as string);
+    zkEVMProvider = new ethers.JsonRpcProvider(process.env.ZK_EVM_RPC as string);
+    deployer = new ethers.Wallet(privateKey, alchemyProvider);
+    zkEvmDeployer = new ethers.Wallet(privateKey, zkEVMProvider);
   } else {
     throw new Error("Network not supported");
   }
-  console.log("Network type:  ", networkType);
+  console.log("Network type: ", networkType);
   console.log(
-    "zkEVM deployer:  ",
+    "zkEVM deployer: ",
     zkEvmDeployer.address,
     "\n",
-    `${networkType} deployer:  `,
+    `${networkType} deployer: `,
     deployer.address
   );
   // MessageSender deploying on Goerli
-  const bridgeSenderFactory =
-    await ethers.getContractFactory(
-      "ApprovalSender",
-      deployer
-    );
-  const bridgeSenderContract =
-    await bridgeSenderFactory.deploy();
+  const bridgeSenderFactory = await ethers.getContractFactory("ApprovalSender", deployer);
+  const bridgeSenderContract = await bridgeSenderFactory.deploy(bridgeAddress);
   await bridgeSenderContract.waitForDeployment();
-  let sednerAddress =
-    await bridgeSenderContract.getAddress();
+  let senderAddress = await bridgeSenderContract.getAddress();
 
-  console.log(
-    "Message sender deployed on: ",
-    sednerAddress
-  );
+  console.log("Message sender deployed on: ", senderAddress);
 
   // MessageReceiver deploying on zkEVM testnet
-  const bridgeReceiverFactory =
-    await ethers.getContractFactory(
-      "MessageReceiver",
-      zkEvmDeployer
-    );
+  const bridgeReceiverFactory = await ethers.getContractFactory("ApprovalReceiver", zkEvmDeployer);
 
-  const bridgeReceiverContract =
-    await bridgeReceiverFactory.deploy();
+  const bridgeReceiverContract = await bridgeReceiverFactory.deploy(bridgeAddress);
   await bridgeReceiverContract.waitForDeployment();
-  let receiverAddress =
-    await bridgeReceiverContract.getAddress();
+  let receiverAddress = await bridgeReceiverContract.getAddress();
 
-  console.log(
-    "Message receiver deployed on: ",
-    receiverAddress
-  );
+  console.log("Message receiver deployed on: ", receiverAddress);
+  console.log("Message sender deployed on: ", senderAddress);
 
   await bridgeSenderContract.setReceiver(receiverAddress);
-  await bridgeReceiverContract.setSender(sednerAddress);
+  await bridgeReceiverContract.setSender(senderAddress);
 
   const outputJson = {
-    messageSenderContract: sednerAddress,
+    messageSenderContract: senderAddress,
     messageReceiverContract: receiverAddress,
   };
 
-  const pathOutputJson = path.join(
-    __dirname,
-    "./senderReceiverOutput.json"
-  );
-  writeFileSync(
-    pathOutputJson,
-    JSON.stringify(outputJson, null, 1)
-  );
+  const pathOutputJson = path.join(__dirname, "./senderReceiverOutput.json");
+  writeFileSync(pathOutputJson, JSON.stringify(outputJson, null, 1));
 }
 
 main().catch((e) => {
--- scripts/sendMessage.ts
+++ scripts/sendMessage.ts
@@ -1,48 +1,37 @@
 import path from "path";
 import { config } from "dotenv";
 //import { ethers } from "hardhat";
-import { JsonRpcProvider, Wallet } from "ethers";
 import { ethers } from "hardhat";
+import { JsonRpcProvider, Wallet } from "ethers";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 config();
 
-const pathSenderReceiverOutput = path.join(
-  __dirname,
-  "../deployment/senderReceiverOutput.json"
-);
-
-const messageSenderContractAddress: string =
-  require(pathSenderReceiverOutput)
-    .messageSenderContract as string;
+const pathSenderReceiverOutput = path.join(__dirname, "../deployment/senderReceiverOutput.json");
+const messageSenderContractAddress: string = require(pathSenderReceiverOutput)
+  .messageSenderContract as string;
 
 const privateKey = process.env.PRIVATE_KEY as string;
+const networkType = process.env.NETWORK_TYPE as string;
 
 async function main() {
-  const alchemyProvider = new ethers.AlchemyProvider(
-    "goerli",
-    process.env.ALCHEMY_API_KEY as string
-  );
-  let deployer = new ethers.Wallet(
-    privateKey,
-    alchemyProvider
-  );
-
-  const bridgeSenderFactory =
-    await ethers.getContractFactory(
-      "ApprovalSender",
-      deployer
-    );
-
-  const bridgeSenderContract =
-    await bridgeSenderFactory.attach(
-      messageSenderContractAddress
-    );
-
-  const tx = await bridgeSenderContract.bridgePingMessage(
-    "test123"
-  );
+  let alchemyProvider: JsonRpcProvider;
+  let deployer: Wallet | HardhatEthersSigner;
+  if (networkType === "testnet") {
+    alchemyProvider = new ethers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY as string);
+    deployer = new ethers.Wallet(privateKey, alchemyProvider);
+  } else if (networkType === "mainnet") {
+    alchemyProvider = new ethers.AlchemyProvider("mainnet", process.env.ALCHEMY_API_KEY as string);
+    deployer = new ethers.Wallet(privateKey, alchemyProvider);
+  } else {
+    [deployer] = await ethers.getSigners();
+    throw new Error("Network not supported");
+  }
+  const bridgeSenderFactory = await ethers.getContractFactory("ApprovalSender", deployer);
+  const bridgeSenderContract = bridgeSenderFactory.attach(messageSenderContractAddress);
+  const tx = await bridgeSenderContract.bridgePingMessage(ethers.ZeroAddress);
 
   console.log(await tx.wait());
-  console.log("Bridge done succesfully");
+  console.log("Message sent succesfully");
 }
 
 main().catch((e) => {
