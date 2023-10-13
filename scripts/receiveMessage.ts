8a2aaadd4193f574feb9372f781ec3e60aea1c68
ThomasAqu1nas <andranikoganyan27@gmail.com>
2023-10-14 01:19:50 +0700

    feat: the first version of the script (unfinished)

--- scripts/receiveMessage.ts
+++ scripts/receiveMessage.ts
@@ -1,1 +1,113 @@
-async function main() {}
+import path from "path";
+import { config } from "dotenv";
+import { ethers } from "hardhat";
+import {
+  DepositsResponse,
+  MerkleProofResponse,
+} from "../types/axios-responses";
+import zkEVMBridgeAbi from "../types/zkEVMBridgeAbi.json";
+import { Wallet, JsonRpcProvider } from "ethers";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import axios, { AxiosResponse } from "axios";
+config();
+
+const pathSenderReceiverOutput = path.join(
+  __dirname,
+  "../deployment/senderReceiverOutput.json"
+);
+
+const messageReceiverContractAddress: string = require(pathSenderReceiverOutput)
+  .messageReceiverContract as string;
+
+const privateKey = process.env.PRIVATE_KEY as string;
+const networkType = process.env.NETWORK_TYPE as string;
+const mainnetBridgeAddress = "0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe";
+const testnetBridgeAddress = "0xF6BEEeBB578e214CA9E23B0e9683454Ff88Ed2A7";
+
+async function main() {
+  let bridgeAddress: string;
+  let zkEVMBridgeContractAddress: string;
+  let baseURL: string;
+  let provider: JsonRpcProvider;
+  let deployer: Wallet | HardhatEthersSigner;
+  if (networkType === "testnet") {
+    provider = new ethers.JsonRpcProvider(
+      process.env.ZK_EVM_TESTNET_RPC as string
+    );
+    deployer = new ethers.Wallet(privateKey, provider);
+    zkEVMBridgeContractAddress = testnetBridgeAddress;
+    baseURL = "https://bridge-api.public.zkevm-test.net";
+  } else if (networkType === "mainnet") {
+    provider = new ethers.JsonRpcProvider(process.env.ZK_EVM_RPC as string);
+    deployer = new ethers.Wallet(privateKey, provider);
+    zkEVMBridgeContractAddress = mainnetBridgeAddress;
+    baseURL = "https://bridge-api.zkevm-rpc.com";
+  } else {
+    [deployer] = await ethers.getSigners();
+    throw new Error("Network not supported");
+  }
+
+  const instance = axios.create({
+    baseURL,
+  });
+
+  const bridgeContractZkeVM = new ethers.Contract(
+    zkEVMBridgeContractAddress,
+    zkEVMBridgeAbi,
+    deployer
+  );
+  console.log("contract: ", bridgeContractZkeVM);
+  const response: AxiosResponse<DepositsResponse> = await instance.get(
+    "/bridges/" + messageReceiverContractAddress,
+    {
+      params: {
+        limit: 100,
+        offset: 0,
+      },
+    }
+  );
+  const depositsArray = response.data.deposits;
+  if (depositsArray.length === 0) {
+    console.log("Not ready yet!");
+    return;
+  }
+
+  for (let i = 0; i < depositsArray.length; i++) {
+    const currentDeposit = depositsArray[i];
+    if (currentDeposit.ready_for_claim) {
+      const proofAxios: AxiosResponse<MerkleProofResponse> = await instance.get(
+        "/merkle-proof",
+        {
+          params: {
+            deposit_ctn: currentDeposit.deposit_cnt,
+            net_id: currentDeposit.orig_net,
+          },
+        }
+      );
+
+      const { proof } = proofAxios.data;
+      const claimTx = await bridgeContractZkeVM.claimMessage(
+        proof.merkle_proof,
+        currentDeposit.deposit_cnt,
+        proof.main_exit_root,
+        proof.rollup_exit_root,
+        currentDeposit.orig_net,
+        currentDeposit.orig_addr,
+        currentDeposit.dest_net,
+        currentDeposit.dest_addr,
+        currentDeposit.amount,
+        currentDeposit.metadata
+      );
+      console.log("claim message succesfully send: ", claimTx.hash);
+      await claimTx.wait();
+      console.log("claim message succesfully mined");
+    } else {
+      console.log("bridge not ready for claim");
+    }
+  }
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
