73c1bd2ae742715282486b87513266433880a1c4
ThomasAqu1nas <andranikoganyan27@gmail.com>
2023-10-18 15:45:43 +0700

    feat: added new fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: "#303030",
+      secondary: "#BB86FC",
+      white: "#FFFFFF",
+      grey: "#7D7D7D",
+      error: "#E01616",
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
 };
