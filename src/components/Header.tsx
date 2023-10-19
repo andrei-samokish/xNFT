726c5a38ac2b810535178ad960e496cab13b59df
4ndrei <andreygames5@mail.ru>
2023-10-19 16:44:08 +0700

    fix: connect button margin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
 	return (
-		<div className="flex flex-row justify-between xl:mr-9">
+		<div className="flex flex-row justify-between">
 			<span className=" bg-secondary w-32 h-10 rounded-3xl flex flex-row gap-2 justify-between p-3 items-center">
 				<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path
