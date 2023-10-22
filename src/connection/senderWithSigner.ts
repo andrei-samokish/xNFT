810933ca0ae295b02a677131c574ad71f913c76b
ThomasAqu1nas <andranikoganyan27@gmail.com>
2023-10-22 17:02:14 +0700

    fix: goerli chain id

--- src/connection/senderWithSigner.ts
+++ src/connection/senderWithSigner.ts
@@ -4,39 +4,39 @@ import getContractSender from "./getContractSender";
 import { ErrorCode, ErrorDescription, ethers } from "ethers";
 
 async function senderWithSigner(networkType: string): Promise<ApprovalSender> {
-	try {
-		await window.ethereum?.request({
-			method: "wallet_switchEthereumChain",
-			params: [{ chainId: ethers.toBeHex(1442) }],
-		});
-	} catch (switchError: any) {
-		// This error code indicates that the chain has not been added to MetaMask.
-		if (switchError.code === 4902) {
-			try {
-				await window.ethereum?.request({
-					method: "wallet_addEthereumChain",
-					params: [
-						{
-							chainId: ethers.toBeHex(1442),
-							chainName: "zkEVM Testnet",
-							rpcUrls: ["https://rpc.public.zkevm-test.net"],
-						},
-					],
-				});
-			} catch (error) {
-				console.log(error);
-			}
-		}
-	}
+  try {
+    await window.ethereum?.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: "0x5" }],
+    });
+  } catch (switchError: any) {
+    // This error code indicates that the chain has not been added to MetaMask.
+    if (switchError.code === 4902) {
+      try {
+        await window.ethereum?.request({
+          method: "wallet_addEthereumChain",
+          params: [
+            {
+              chainId: "0x5",
+              chainName: "zkEVM Testnet",
+              rpcUrls: ["https://rpc.public.zkevm-test.net"],
+            },
+          ],
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 
-	await metamaskProvider?.send("eth_requestAccounts", []);
-	const signer = await metamaskProvider?.getSigner();
+  await metamaskProvider?.send("eth_requestAccounts", []);
+  const signer = await metamaskProvider?.getSigner();
 
-	if (signer) {
-		return getContractSender(networkType).connect(signer) as ApprovalSender;
-	} else {
-		throw new Error("Access to the wallet is denied:");
-	}
+  if (signer) {
+    return getContractSender(networkType).connect(signer) as ApprovalSender;
+  } else {
+    throw new Error("Access to the wallet is denied:");
+  }
 }
 
 export default senderWithSigner;
